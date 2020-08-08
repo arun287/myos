62a3f5d5b294e3532d54467cfef0880e4c2f9148
arun287 <arunravi789@gmail.com>
2020-08-08 11:44:28 +0530

    Fixed discrepancy between IRQ and ISR

--- kernel/arch/i386/interrupts.s
+++ kernel/arch/i386/interrupts.s
@@ -1,37 +1,37 @@
 .global flush_idt
 
-.global irq0
-.global irq1
-.global irq2
-.global irq3
-.global irq4
-.global irq5
-.global irq6
-.global irq7
-.global irq8
-.global irq9
-.global irq10
-.global irq11
-.global irq12
-.global irq13
-.global irq14
-.global irq15
-.global irq16
-.global irq17
-.global irq18
-.global irq19
-.global irq20
-.global irq21
-.global irq22
-.global irq23
-.global irq24
-.global irq25
-.global irq26
-.global irq27
-.global irq28
-.global irq29
-.global irq30
-.global irq31
+.global isr0
+.global isr1
+.global isr2
+.global isr3
+.global isr4
+.global isr5
+.global isr6
+.global isr7
+.global isr8
+.global isr9
+.global isr10
+.global isr11
+.global isr12
+.global isr13
+.global isr14
+.global isr15
+.global isr16
+.global isr17
+.global isr18
+.global isr19
+.global isr20
+.global isr21
+.global isr22
+.global isr23
+.global isr24
+.global isr25
+.global isr26
+.global isr27
+.global isr28
+.global isr29
+.global isr30
+.global isr31
 
 .extern irq0_handler
 .extern irq1_handler
@@ -66,224 +66,224 @@
 .extern irq30_handler
 .extern irq31_handler
 
-irq0:
+isr0:
 	pusha
 	cld
 	call irq0_handler
 	popa
 	iret
  
-irq1:
+isr1:
 	pusha
 	cld
 	call irq1_handler
 	popa
 	iret
  
-irq2:
+isr2:
 	pusha
 	cld
 	call irq2_handler
 	popa
 	iret
  
-irq3:
+isr3:
 	pusha
 	cld
 	call irq3_handler
 	popa
 	iret
  
-irq4:
+isr4:
 	pusha
 	cld
 	call irq4_handler
 	popa
 	iret
  
-irq5:
+isr5:
 	pusha
 	cld
 	call irq5_handler
 	popa
 	iret
  
-irq6:
+isr6:
 	pusha
 	cld
 	call irq6_handler
 	popa
 	iret
  
-irq7:
+isr7:
 	pusha
 	cld
 	call irq7_handler
 	popa
 	iret
  
-irq8:
+isr8:
 	pusha
 	cld
 	call irq8_handler
 	popa
 	iret
  
-irq9:
+isr9:
 	pusha
 	cld
 	call irq9_handler
 	popa
 	iret
  
-irq10:
+isr10:
 	pusha
 	cld
 	call irq10_handler
 	popa
 	iret
  
-irq11:
+isr11:
 	pusha
 	cld
 	call irq11_handler
 	popa
 	iret
  
-irq12:
+isr12:
 	pusha
 	cld
 	call irq12_handler
 	popa
 	iret
  
-irq13:
+isr13:
 	pusha
 	cld
 	call irq13_handler
 	popa
 	iret
  
-irq14:
+isr14:
 	pusha
 	cld
 	call irq14_handler
 	popa	
 	iret
  
-irq15:
+isr15:
 	pusha
 	cld
 	call irq15_handler
 	popa	
 	iret
 
-irq16:
+isr16:
 	pusha
 	cld
 	call irq16_handler
 	popa
 	iret
  
-irq17:
+isr17:
 	pusha
 	cld
 	call irq17_handler
 	popa
 	iret
  
-irq18:
+isr18:
 	pusha
 	cld
 	call irq18_handler
 	popa
 	iret
  
-irq19:
+isr19:
 	pusha
 	cld
 	call irq19_handler
 	popa
 	iret
  
-irq20:
+isr20:
 	pusha
 	cld
 	call irq20_handler
 	popa
 	iret
  
-irq21:
+isr21:
 	pusha
 	cld
 	call irq21_handler
 	popa
 	iret
  
-irq22:
+isr22:
 	pusha
 	cld
 	call irq22_handler
 	popa
 	iret
  
-irq23:
+isr23:
 	pusha
 	cld
 	call irq23_handler
 	popa
 	iret
  
-irq24:
+isr24:
 	pusha
 	cld
 	call irq24_handler
 	popa
 	iret
  
-irq25:
+isr25:
 	pusha
 	cld
 	call irq25_handler
 	popa
 	iret
  
-irq26:
+isr26:
 	pusha
 	cld
 	call irq26_handler
 	popa
 	iret
  
-irq27:
+isr27:
 	pusha
 	cld
 	call irq27_handler
 	popa
 	iret
  
-irq28:
+isr28:
 	pusha
 	cld
 	call irq28_handler
 	popa
 	iret
  
-irq29:
+isr29:
 	pusha
 	cld
 	call irq29_handler
 	popa
 	iret
  
-irq30:
+isr30:
 	pusha
 	cld
 	call irq30_handler
 	popa	
 	iret
  
-irq31:
+isr31:
 	pusha
 	cld
 	call irq31_handler
