13de561c9a0d40bde820745d41e29f993babc14b
arun <arunravi789@gmail.com>
2020-09-01 19:29:51 +0530

    Corrected Paging code to use correct number of frames

--- kernel/arch/i386/paging.c
+++ kernel/arch/i386/paging.c
@@ -7,10 +7,11 @@
 #include "../../../libc/include/stdio.h"
 #include "../../include/kernel/irq.h"
 
-#define INDEX_FROM_BIT(a) (a/32)
-#define OFFSET_FROM_BIT(a) (a%32)
+//Wordsize of bitmap is 32
+#define WORD_OFFSET(a) (a/32)
+#define BIT_OFFSET(a) (a%32)
 
-uint32_t *frames;
+uint32_t *frames_bitmap;
 uint32_t nframes;
 
 extern uint32_t placement_address;
@@ -21,36 +22,39 @@ page_directory_t *current_directory = 0;
 static void set_frame(uint32_t frame_addr)
 {
     uint32_t frame = frame_addr/0x1000;
-    uint32_t index = INDEX_FROM_BIT(frame);
-    uint32_t offset = OFFSET_FROM_BIT(frame);
-    frames[index] |= (1<<offset);
+    uint32_t index = WORD_OFFSET(frame);
+    uint32_t offset = BIT_OFFSET(frame);
+
+    frames_bitmap[index] |= (1<<offset);
 }
 
 static void clear_frame(uint32_t frame_addr)
 {
     uint32_t frame = frame_addr/0x1000;
-    uint32_t index = INDEX_FROM_BIT(frame);
-    uint32_t offset = OFFSET_FROM_BIT(frame);
-    frames[index] &= ~(1<<offset);
+    uint32_t index = WORD_OFFSET(frame);
+    uint32_t offset = BIT_OFFSET(frame);
+
+    frames_bitmap[index] &= ~(1<<offset);
 }
 
 static uint32_t check_frame(uint32_t frame_addr)
 {
     uint32_t frame = frame_addr/0x1000;
-    uint32_t index = INDEX_FROM_BIT(frame);
-    uint32_t offset = OFFSET_FROM_BIT(frame);
-    return (frames[index] & (1<<offset));
+    uint32_t index = WORD_OFFSET(frame);
+    uint32_t offset = BIT_OFFSET(frame);
+
+    return (frames_bitmap[index] & (1<<offset));
 }
 
 static uint32_t first_free_frame()
 {
-    for(uint32_t i = 0; i < INDEX_FROM_BIT(nframes); i++)
+    for(uint32_t i = 0; i < WORD_OFFSET(nframes); i++)
     {
-        if(frames[i] != 0xFFFFFFFF)
+        if(frames_bitmap[i] != 0xFFFFFFFF)
         {
             for(uint32_t j = 0; j<32; j++)
             {
-                if(!(frames[i] & ((1<<j))))
+                if(!(frames_bitmap[i] & ((1<<j))))
                     return 32*i + j;
             }
         }
@@ -95,17 +99,18 @@ void free_frame(page_t *page)
 
 void init_paging()
 {
-    uint32_t mem_end_page = 0x1000000;
-    nframes = mem_end_page/0x1000;
+    uint32_t mem_end_page = 0x1000000;   //16 MB memory assumed
+    nframes = mem_end_page/0x1000;       //Each frame is 4096 bits big
 
-    frames = (uint32_t *)kmalloc(INDEX_FROM_BIT(nframes), 0, 0);
-    memset(frames, 0, INDEX_FROM_BIT(nframes));
+    //There are 4096 4KB frames in 16MB of memory
+    frames_bitmap = (uint32_t *)kmalloc(WORD_OFFSET(nframes), 0, 0);
+    memset(frames_bitmap, 0, WORD_OFFSET(nframes)*4);
     
     kernel_directory = (page_directory_t *)kmalloc(sizeof(page_directory_t),1,0);
     memset(kernel_directory, 0, sizeof(page_directory_t));
     current_directory = kernel_directory;
 
-    uint32_t i=0x00;
+    uint32_t i=0x00000000;
     while(i<placement_address)
     { 
         alloc_frame(get_page(i, 1, kernel_directory), 0, 0);
@@ -119,11 +124,11 @@ void init_paging()
 void switch_page_dir(page_directory_t *dir)
 {
     current_directory = dir;
-    asm volatile("mov %0, %%cr3":: "r"(&dir->tablesPhy));
+    asm ("mov %0, %%cr3":: "r"(&dir->tablesPhy));
     uint32_t cr0;
-    asm volatile("mov %%cr0, %0": "=r"(cr0));
+    asm ("mov %%cr0, %0": "=r"(cr0));
     cr0 |= 0x80000000;
-    asm volatile("mov %0, %%cr0":: "r"(cr0));
+    asm ("mov %0, %%cr0":: "r"(cr0));
 }
 
 page_t *get_page(uint32_t address, int make, page_directory_t *dir)
